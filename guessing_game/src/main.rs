f24f9787500db07fc4610b111d6b992eadacac24
Steven Bustillo <sb.sys.dev@gmail.com>
2024-08-08 22:36:31 +0000

    :sparkles: - guessing game :hammer: - added devcontainer config files :tada:

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+use rand::Rng;
+use std::{cmp::Ordering, io};
+
+fn main() {
+    let secret_number: u32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("Guess the number!\n");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        print!("You guessed: {guess} -> ");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+
+                break;
+            }
+        }
+
+        println!();
+    }
+}
